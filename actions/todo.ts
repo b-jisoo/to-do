8b930df9b834468e570f42eb83bd76aaa3106c59
b-jisoo <mrk334kr@naver.com>
2023-04-14 16:24:15 +0900

    refactor: todo Actions 최적화

--- actions/todo.ts
+++ actions/todo.ts
@@ -5,43 +5,54 @@ import {
   TodoList,
   todoListState,
 } from "@/recoil/ToDo";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { useRecoilState } from "recoil";
 
 const useToDoActions = () => {
   const [toDoItem, setToDoItem] = useRecoilState(todoItemState);
-  const [TodoList, setTodoList] = useRecoilState(todoListState);
-  const ToDoActions = useMemo(
-    () => ({
-      getItem: () => {
-        return toDoItem;
-      },
-      addItem: (todoItemInputs: IToDoItem) => {
-        setToDoItem((oldTodoItem) => [...oldTodoItem, todoItemInputs]);
-      },
-      deleteItem: () => {
-        const newTodoItem = toDoItem.filter(
-          (item) => item.isComplete === false
-        );
-        setToDoItem(newTodoItem);
-      },
-      getList: () => {
-        return TodoList;
-      },
-      addList: (todoListInputs: TodoList) => {
-        setTodoList((oldTodoList) => [...oldTodoList, todoListInputs]);
-      },
-      deleteList: (id: string) => {
-        const removeTodoList = TodoList.filter((list) => list.id !== id);
-        const removeTodoItem = toDoItem.filter((item) => item.listId !== id);
-        setTodoList(removeTodoList);
-        setToDoItem(removeTodoItem);
-      },
-    }),
-    [toDoItem, setToDoItem, TodoList, setTodoList]
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+
+  const getItem = useCallback(() => toDoItem, [toDoItem]);
+  const addItem = useCallback(
+    (todoItemInputs: IToDoItem) => {
+      setToDoItem((prevTodoItem) => [...prevTodoItem, todoItemInputs]);
+    },
+    [setToDoItem]
+  );
+  const deleteItem = useCallback(() => {
+    const newTodoItem = toDoItem.filter((item) => !item.isComplete);
+    setToDoItem(newTodoItem);
+  }, [toDoItem, setToDoItem]);
+  const getList = useCallback(() => todoList, [todoList]);
+  const addList = useCallback(
+    (todoListInputs: TodoList) => {
+      setTodoList((prevTodoList) => [...prevTodoList, todoListInputs]);
+    },
+    [setTodoList]
+  );
+  const deleteList = useCallback(
+    (id: string) => {
+      setTodoList((prevTodoList) =>
+        prevTodoList.filter((list) => list.id !== id)
+      );
+      setToDoItem((prevTodoItem) =>
+        prevTodoItem.filter((item) => item.listId !== id)
+      );
+    },
+    [setTodoList, setToDoItem]
   );
 
-  return ToDoActions;
+  return useMemo(
+    () => ({
+      getItem,
+      addItem,
+      deleteItem,
+      getList,
+      addList,
+      deleteList,
+    }),
+    [getItem, addItem, deleteItem, getList, addList, deleteList]
+  );
 };
 
 export default useToDoActions;
